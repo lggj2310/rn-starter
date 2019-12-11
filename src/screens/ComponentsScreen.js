eadcda6f0c2593eeeee4900229b3c4239261141c
luis.gudiel <lggj2310@gmail.com>
2019-12-11 16:26:27 -0600

    Buttons and starting with navigation

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-
-    const greeting = <Text>Hello to you!</Text>;
+    const name = "Luis";
 
 	return (
         <View>
-            <Text style={styles.textStyle}>This is the components screen</Text>
-            <Text>{greeting}</Text>
+            <Text style={styles.headerStyle}>Getting started with React Native</Text>
+            <Text style={styles.subHeaderStyle}>Ny name is {name}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-	textStyle: {
-		fontSize: 30
-	}
+	headerStyle: {
+		fontSize: 45
+    },
+    subHeaderStyle: {
+        fontSize: 20
+    }
 });
 
 export default ComponentsScreen;
